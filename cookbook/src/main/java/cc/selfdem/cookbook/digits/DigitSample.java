3780db57e3fe13c0b6215a82f728ee238337aa86
chenchao <chenchao@dragonest.com>
2017-10-24 16:43:16 +0800

    添加 第7节 用下划线分隔数字,提高可读性

---
+++ cookbook/src/main/java/cc/selfdem/cookbook/digits/DigitSample.java
@@ -0,0 +1,9 @@
+package cc.selfdem.cookbook.digits;
+
+public class DigitSample {
+
+    public static void main(String[] args) {
+        int oneMillion = 1_000_000;
+        System.out.println(oneMillion);
+    }
+}
